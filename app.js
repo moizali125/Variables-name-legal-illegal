7de9583a00b5f2ed67496295958597a281d15728
moizali125 <ha9913083@gmail.com>
2024-03-14 23:17:11 +0500

    Variables-name-legal-illegal

---
+++ app.js
@@ -0,0 +1,17 @@
+// var heading = 'Rule For naming JS variable\r\r\r'
+// var stateone = 'Varable name can only contain ,numbers, $ and _.For example : $my_1stVarable'
+// var statethree = 'Varable name are case sensitive'
+// var statefore = 'Varable name should not be JS keywords'
+// var statetwo = 'Variable mush begin with a letter, $ and _.For example : $name, _name or name'
+// var br = '\r'
+// document.write(heading)
+// document.write(stateone)
+// document.write(br)
+// document.write(statetwo)
+// document.write(br)
+// document.write(statethree)
+// document.write(br)
+// document.write(statefore)
+// var text = 'Rule For naming JS variable \r\r\r\r Varable name can only contain ,numbers, $ and _.For example : $my_1stVarable\rVariable mush begin with a letter, $ and _.For example : $name, _name or name\rVarable name are case sensitive\rVarable name should not be JS keywords'
+document.write('Rule For naming JS variable \r\r\r\r Varable name can only contain ,numbers, $ and _.For example : $my_1stVarable\rVariable mush begin with a letter, $ and _.For example : $name, _name or name\rVarable name are case sensitive\rVarable name should not be JS keywords')
+alert('Rule For naming JS variable \r\r Varable name can only contain ,numbers, $ and _.For example : $my_1stVarable\rVariable mush begin with a letter, $ and _.For example : $name, _name or name\rVarable name are case sensitive\rVarable name should not be JS keywords')
